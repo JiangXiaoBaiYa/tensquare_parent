0065bcf78fc0424eca3f1e98ac10e6ecc3ba2690
JiangGuangming <735825678@qq.com>
2019-08-24 23:36:11 +0800

    统一异常处理类的编写

---
+++ tensquare_base/src/main/java/com/tensquare/base/controller/BaseExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.tensquare.base.controller;
+
+/**
+ * @Author: 姜光明
+ * @Date: 2019/8/24 23:32
+ */
+
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理类
+ */
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR, e.getMessage())
+    }
+}
